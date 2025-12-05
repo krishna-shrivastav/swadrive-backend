bf1417b0ed850ed4f4ac45aa66aebc80d573f096
Krishna Shrivastav <krishanashrivastav9@gmail.com>
2025-12-05 17:45:42 +0530

    Update db.js

--- db.js
+++ db.js
@@ -3,10 +3,11 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'swadrive_db',
+  host: process.env.DB_HOST,      // Railway Host
+  user: process.env.DB_USER,      // root
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,  // railway
+  port: process.env.DB_PORT,      // << IMPORTANT ( Railway port )
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
