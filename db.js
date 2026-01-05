628560daa8bdcc6d47b8bd9da4ad3a9ae6964755
Krishna Shrivastav <krishanashrivastav9@gmail.com>
2026-01-05 19:08:43 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  connectionString: process.env.postgresql://postgres:swadrive@123@db.vquvwrrzdbvxjxnkqyih.supabase.co:5432/postgres,
+  connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false }
 });
 
