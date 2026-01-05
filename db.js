82f630d55ffb866d146acd585f2421ab494864c1
Krishna Shrivastav <krishanashrivastav9@gmail.com>
2026-01-05 18:56:59 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,16 +1,8 @@
-// db.js
-const mysql = require('mysql2/promise');
-require('dotenv').config();
+const { Pool } = require("pg");
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,      // Railway Host
-  user: process.env.DB_USER,      // root
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,  // railway
-  port: process.env.DB_PORT,      // << IMPORTANT ( Railway port )
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
+const pool = new Pool({
+  connectionString: process.env.postgresql://postgres:swadrive@123@db.vquvwrrzdbvxjxnkqyih.supabase.co:5432/postgres,
+  ssl: { rejectUnauthorized: false }
 });
 
 module.exports = pool;
